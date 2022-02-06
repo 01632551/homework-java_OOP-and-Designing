3a9d554a3a5152ed0c4a6c46f18c46ca72b7bc81
Egor) <kes2079531@gmail.com>
2022-02-06 21:20:18 +0300

    update tests

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RadioTest {
+class RadioWithParameterInConstructorTest {
     private Radio radio = new Radio(20);
 
 // TEST1: DECREASE CURRENT RADIO STATION
@@ -69,7 +69,7 @@ class RadioTest {
         assertEquals(expected, radio.getCurrentVolume());
     }
 
-// TEST6: SET CURRENT RADIO STATION
+// TEST5: SET CURRENT RADIO STATION
     @ParameterizedTest
     @CsvSource(value = {"min value | 0 | 0", "above min value per 1 | 1 | 1",
             "max value | 10 | 10", "below max value per 1 | 9 | 9",
@@ -83,7 +83,7 @@ class RadioTest {
         assertEquals(expected, radio.getCurrentRadioStation());
     }
 
-// TEST7: NOT SET CURRENT RADIO STATION
+// TEST6: NOT SET CURRENT RADIO STATION
     @ParameterizedTest
     @CsvSource(value = {"below min value per 1 | -1 | 0",
             "above max value per 1 | 20 | 0",
@@ -97,7 +97,7 @@ class RadioTest {
         assertEquals(expected, radio.getCurrentRadioStation());
     }
 
-// TEST8: SET CURRENT VOLUME
+// TEST7: SET CURRENT VOLUME
     @ParameterizedTest
     @CsvSource(value = {"min value | 0 | 0", "above min value per 1 | 1 | 1",
             "max value | 100 | 100", "below max value per 1 | 99 | 99",
@@ -111,7 +111,7 @@ class RadioTest {
         assertEquals(expected, radio.getCurrentVolume());
     }
 
-// TEST9: NOT SET CURRENT VOLUME
+// TEST8: NOT SET CURRENT VOLUME
     @ParameterizedTest
     @CsvSource(value = {"below min value per 1 | -1 | 0",
             "above max value per 1 | 101 | 0",
@@ -120,6 +120,127 @@ class RadioTest {
     /* below min value per 1 (- 1)
     * above max value per 1 (+ 1)
     * some number not in the boards of values*/
+    public void shouldNotSetCurrentVolume(String testName, int currentVolume, int expected) {
+        radio.setCurrentVolume(currentVolume);
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+}
+
+class RadioWithDefaultParameterTest {
+    private Radio radio = new Radio();
+
+// TEST1: DECREASE CURRENT RADIO STATION
+    @ParameterizedTest
+    @CsvSource(value = {"min value | 0 | 9", "above min value per 1 | 1 | 0",
+            "max value | 9 | 8", "below max value per 1 | 8 | 7",
+            "in middle area of values | 5 | 4"}, delimiter = '|')
+
+    /* min value | above min value per 1 (+ 1)
+     * max value | below max value per 1 (- 1)
+     * in middle area of values */
+
+    public void shouldDecreaseCurrentRadioStation(String testName, int currentRadioStation, int expected) {
+        radio.setCurrentRadioStation(currentRadioStation);
+        radio.prevRadioStation();
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+
+// TEST2: INCREASE CURRENT RADIO STATION
+    @ParameterizedTest
+    @CsvSource(value = {"min value | 0 | 1", "above min value per 1 | 1 | 2",
+            "max value | 9 | 0", "below max value per 1 | 8 | 9",
+            "in middle area of values | 4 | 5"}, delimiter = '|')
+
+    /* min value | above min value per 1 (+ 1)
+     * max value | below max value per 1 (- 1)
+     * in middle area of values */
+    public void shouldIncreaseCurrentRadioStation(String testName, int currentRadioStation, int expected) {
+        radio.setCurrentRadioStation(currentRadioStation);
+        radio.nextRadioStation();
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+
+// TEST3: DECREASE CURRENT VOLUME
+    @ParameterizedTest
+    @CsvSource(value = {"min value | 0 | 0", "above min value per 1 | 1 | 0",
+            "max value | 10 | 9", "below max value per 1 | 9 | 8",
+            "in middle area of values | 6 | 5"}, delimiter = '|')
+
+    /* min value | above min value per 1 (+ 1)
+     * max value | below max value per 1 (- 1)
+     * in middle area of values */
+    public void shouldDecreaseCurrentVolume(String testName, int currentVolume, int expected) {
+        radio.setCurrentVolume(currentVolume);
+        radio.prevVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+// TEST4: INCREASE CURRENT VOLUME
+    @ParameterizedTest
+    @CsvSource(value = {"min value | 0 | 1", "above min value per 1 | 1 | 2",
+            "max value | 100 | 100", "below max value per 1 | 99 | 100",
+            "in middle area of values | 38 | 39"}, delimiter = '|')
+
+    /* min value | above min value per 1 (+ 1)
+     * max value | below max value per 1 (- 1)
+     * in middle area of values */
+    public void shouldIncreaseCurrentVolume(String testName, int currentVolume, int expected) {
+        radio.setCurrentVolume(currentVolume);
+        radio.nextVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+// TEST5: SET CURRENT RADIO STATION
+    @ParameterizedTest
+    @CsvSource(value = {"min value | 0 | 0", "above min value per 1 | 1 | 1",
+            "max value | 9 | 9", "below max value per 1 | 8 | 8",
+            "in middle area of values | 7 | 7"}, delimiter = '|')
+
+    /* min value | above min value per 1 (- 1)
+     * max value | below max value per 1 (+ 1)
+     * in middle area of values*/
+    public void shouldSetCurrentRadioStation(String testName, int currentRadioStation, int expected) {
+        radio.setCurrentRadioStation(currentRadioStation);
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+
+// TEST6: NOT SET CURRENT RADIO STATION
+    @ParameterizedTest
+    @CsvSource(value = {"below min value per 1 | -1 | 0",
+            "above max value per 1 | 10 | 0",
+            "some number not in the boards of values | -123 | 0"}, delimiter = '|')
+
+    /* below min value per 1 (- 1)
+     * above max value per 1 (+ 1)
+     * some number not in the boards of values*/
+    public void shouldNotSetCurrentRadioStation(String testName, int currentRadioStation, int expected) {
+        radio.setCurrentRadioStation(currentRadioStation);
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+
+// TEST7: SET CURRENT VOLUME
+    @ParameterizedTest
+    @CsvSource(value = {"min value | 0 | 0", "above min value per 1 | 1 | 1",
+            "max value | 10 | 10", "below max value per 1 | 9 | 9",
+            "in middle area of values | 3 | 3"}, delimiter = '|')
+
+    /* min value | above min value per 1 (- 1)
+     * max value | below max value per 1 (+ 1)
+     * in middle area of values*/
+    public void shouldSetCurrentVolume(String testName, int currentVolume, int expected) {
+        radio.setCurrentVolume(currentVolume);
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+// TEST8: NOT SET CURRENT VOLUME
+    @ParameterizedTest
+    @CsvSource(value = {"below min value per 1 | -1 | 0",
+            "above max value per 1 | 101 | 0",
+            "some number not in the boards of values | 342 | 0"}, delimiter = '|')
+
+    /* below min value per 1 (- 1)
+     * above max value per 1 (+ 1)
+     * some number not in the boards of values*/
     public void shouldNotSetCurrentVolume(String testName, int currentVolume, int expected) {
         radio.setCurrentVolume(currentVolume);
         assertEquals(expected, radio.getCurrentVolume());
